3494db59050e3c1564173b0f6dc68477c898973f
Ben Rugg <ben@theregime.com>
2023-10-12 09:16:43 -0500

    Add app check to firebase functions

--- firebase-functions/images/generate-image-call.js
+++ firebase-functions/images/generate-image-call.js
@@ -1,16 +1,11 @@
 import { onCall, HttpsError } from 'firebase-functions/v2/https'
-// import { getFirestore, Timestamp } from 'firebase-admin/firestore'
 import { generateImage } from './include/generate-image.js'
 
-// export const generateimagecall = onCall({ enforceAppCheck: true, timeoutSeconds: 90, secrets: ['STABILITY_API_KEY'] }, async (request) => {
 export const generateimagecall = onCall(
-  { timeoutSeconds: 90, secrets: ['STABILITY_API_KEY'] },
+  { enforceAppCheck: true, timeoutSeconds: 90, secrets: ['STABILITY_API_KEY'] },
   async (request) => {
     // validate the request
-    // TODO: do this with an auth include (like in Daily Good)
-    // if (!request.auth) {
-    //   throw new HttpsError('unauthenticated', 'The function must be called while authenticated.')
-    // }
+    // TODO: add more validation
     if (!request.data.prompt) {
       throw new HttpsError(
         'invalid-argument',
@@ -23,20 +18,5 @@ export const generateimagecall = onCall(
 
     // return the response
     return response
-
-    // // init firestore
-    // const db = getFirestore()
-
-    // // insert the prompt into the generatedImages collection
-    // const generatedImagesRef = db.collection('generatedImages')
-    // const newGeneratedImageRef = generatedImagesRef.doc()
-    // await newGeneratedImageRef.set({
-    //   prompt: prompt,
-    //   url: `https://picsum.photos/seed/${Math.floor(Math.random() * 1000)}/1600/900`,
-    //   createdAt: Timestamp.now()
-    // })
-
-    // // return success
-    // return { status: 'success' }
   }
 )
