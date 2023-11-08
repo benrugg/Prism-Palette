de0472f271589fd713f3654e294c0514e49c7df0
Ben Rugg <ben@theregime.com>
2023-11-08 09:36:21 -0600

    Fix style of favorite icon on selected image

--- src/components/ImageWithInfo.vue
+++ src/components/ImageWithInfo.vue
@@ -3,31 +3,7 @@
     <Transition name="medium-fade">
       <img :src="image.url" v-show="isLoaded" @load="isLoaded = true" />
     </Transition>
-    <div class="info">
-      <div v-if="originalPositivePrompts.length > 1">
-        <strong>Prompts:</strong>
-        <ul>
-          <li v-for="(prompt, i) in originalPositivePrompts" :key="`${prompt}${i}`">
-            {{ prompt.prompt }} {{ prompt.weight == 1 ? '' : `(weight: ${prompt.weight})` }}
-          </li>
-        </ul>
-      </div>
-      <div v-else>
-        <strong>Prompt:</strong> {{ originalPositivePrompts[0].prompt }}
-        {{
-          originalPositivePrompts[0].weight == 1
-            ? ''
-            : `(weight: ${originalPositivePrompts[0].weight})`
-        }}
-      </div>
-      <p><strong>Preset:</strong> {{ presetStyleName }}</p>
-      <p><strong>Created:</strong> {{ createdAtDate }}</p>
-    </div>
-    <div class="favoriteIcon" @click="toggleFavorite">
-      <span class="material-symbols-outlined thicker" :class="{ filled: isFavorite }"
-        >favorite</span
-      >
-    </div>
+
     <div
       class="arrowButton left"
       :class="{ disabled: isAtBeginning }"
@@ -43,6 +19,34 @@
       @dblclick.stop="void 0"
     >
       <span class="material-symbols-outlined">arrow_circle_right</span>
+    </div>
+
+    <div class="info">
+      <div class="textContainer">
+        <div v-if="originalPositivePrompts.length > 1">
+          <strong>Prompts:</strong>
+          <ul>
+            <li v-for="(prompt, i) in originalPositivePrompts" :key="`${prompt}${i}`">
+              {{ prompt.prompt }} {{ prompt.weight == 1 ? '' : `(weight: ${prompt.weight})` }}
+            </li>
+          </ul>
+        </div>
+        <div v-else>
+          <strong>Prompt:</strong> {{ originalPositivePrompts[0].prompt }}
+          {{
+            originalPositivePrompts[0].weight == 1
+              ? ''
+              : `(weight: ${originalPositivePrompts[0].weight})`
+          }}
+        </div>
+        <p><strong>Preset:</strong> {{ presetStyleName }}</p>
+        <p><strong>Created:</strong> {{ createdAtDate }}</p>
+      </div>
+      <div class="actionContainer">
+        <div class="favoriteIcon" @click="toggleFavorite">
+          <span class="material-symbols-outlined" :class="{ filled: isFavorite }">favorite</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -152,41 +156,48 @@ export default {
     position: absolute;
     bottom: 0;
     left: 0;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
     width: 100%;
     padding: 0.8rem 1.15rem;
     background-color: rgba(0, 0, 0, 0.6);
 
-    div,
-    p {
-      font-size: 0.9rem;
-      color: #ffffff;
+    .textContainer {
+      flex-grow: 1;
 
-      strong {
-        font-weight: 600;
-        color: inherit;
+      div,
+      p {
+        font-size: 0.9rem;
+        color: #ffffff;
+
+        strong {
+          font-weight: 600;
+          color: inherit;
+        }
+      }
+
+      ul {
+        list-style: disc;
+        margin-inline-start: 1.5rem;
       }
     }
 
-    ul {
-      list-style: disc;
-      margin-inline-start: 1.5rem;
-    }
-  }
+    .actionContainer {
+      flex-grow: 0;
 
-  .favoriteIcon {
-    position: absolute;
-    bottom: 0.08rem;
-    right: 0.4rem;
-    color: #ffffff;
-    line-height: 1em;
-    font-size: 1.8rem;
-    text-shadow: 0 0 0.3rem rgba(0, 0, 0, 0.4);
-    cursor: pointer;
-    transition: opacity 0.2s ease-in-out;
-    opacity: 0;
+      .favoriteIcon {
+        color: #ffffff;
+        line-height: 1em;
+        font-size: 2.2rem;
+        text-shadow: 0 0 0.3rem rgba(0, 0, 0, 0.4);
+        cursor: pointer;
 
-    span {
-      font-size: inherit;
+        span {
+          display: block;
+          font-size: inherit;
+        }
+      }
     }
   }
 
@@ -217,13 +228,6 @@ export default {
       opacity: 0.4;
       cursor: default;
       pointer-events: none;
-    }
-  }
-
-  &.isLoaded:hover,
-  &.isLoaded.isFavorite {
-    .favoriteIcon {
-      opacity: 1;
     }
   }
 }
